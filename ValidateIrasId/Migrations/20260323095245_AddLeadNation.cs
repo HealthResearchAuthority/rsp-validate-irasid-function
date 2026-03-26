3577548c30b0d91bd40e4981d40fe66a033e3e67
vinaylingappa-HRA <vinay.lingappa_PA@hra.nhs.uk>
2026-03-26 09:51:00 +0000

    Revert "Revert "Added LeadNation column to the data model (#9)" (#10)"
    This reverts commit ef87dceb582acfc16e2d3861e0ed0d41066ff0aa.

---
+++ ValidateIrasId/Migrations/20260323095245_AddLeadNation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ValidateIrasId.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLeadNation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LeadNation",
+                table: "HarpProjectRecords",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LeadNation",
+                table: "HarpProjectRecords");
+        }
+    }
+}
