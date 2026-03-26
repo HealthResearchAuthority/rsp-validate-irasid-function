a29ab3c06e10ec5963e7d90975f3d96756ef19c4
vinaylingappa-HRA <vinay.lingappa_PA@hra.nhs.uk>
2026-03-26 09:38:52 +0000

    Added LeadNation column to the data model (#9)

--- ValidateIrasId/Migrations/20260323095245_AddLeadNation.Designer.cs
+++ ValidateIrasId/Migrations/20260323095245_AddLeadNation.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ValidateIrasId.Infrastructure;
 
 #nullable disable
 
-namespace ValidateIrasId.Infrastructure.Migrations
+namespace ValidateIrasId.Migrations
 {
     [DbContext(typeof(HarpProjectDataDbContext))]
-    partial class HarpProjectDataDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260323095245_AddLeadNation")]
+    partial class AddLeadNation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
